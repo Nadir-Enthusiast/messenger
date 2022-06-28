6c748944a0b6e96005f56d30fa3628006643a044
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-28 13:22:00 -0400

    Create SidebarChat.js

---
+++ day2/SidebarChat.js
@@ -0,0 +1,16 @@
+import "./SidebarChat.css"
+import React from 'react'
+
+function SidebarChat() {
+  return (
+    <div className="sidebar-chat">
+        {/* avatar */}
+        <div className="chat-info-container">
+            <h2>Username</h2>
+            <p>Last Message</p>
+        </div>
+    </div>
+  )
+}
+
+export default SidebarChat
