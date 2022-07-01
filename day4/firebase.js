0e5b5a3d6c166e8d6be36ba71f854b4e291b9883
Nadir <106572725+Nadir-Enthusiast@users.noreply.github.com>
2022-06-30 22:24:03 -0400

    Create firebase.js

---
+++ day4/firebase.js
@@ -0,0 +1,10 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  //firebase config
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export {app, analytics};
